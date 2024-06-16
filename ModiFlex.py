a8468655f5b4bb8470e1c29951bc8ed8541f8b0c
GiggySauce <36463235+GiggySauce@users.noreply.github.com>
2024-06-15 22:27:45 -0400

    Update ModiFlex.py

--- ModiFlex.py
+++ ModiFlex.py
@@ -1,14 +1,14 @@
 bl_info = {
         "name": "ModiFlex",
-        "description": "Simplifies object management by clearing vertex groups and modifiers with ease.",
+        "description": "Simplifies object management by clearing vertex groups, modifiers, and shapekeys with ease.",
         "author": "Darber",
-        "version": (1, 0),
-        "blender": (4, 0, 0),
+        "version": (1, 1),
+        "blender": (4, 1, 0),
         "location": "3D View > Properties > Tool > ModiFlex",
         "warning": "", # used for warning icon and text in add-ons panel
         "tracker_url": "https://github.com/GiggySauce/ModiFlex/issues",
         "support": "COMMUNITY",
-        "category": "Tool"
+        "category": "Tools"
         }
 
 import bpy
@@ -18,12 +18,13 @@ class OBJECT_PT_ClearVertexGroupsPanel(bpy.types.Panel):
     bl_idname = "OBJECT_PT_clear_vertex_groups"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = 'Tool'
+    bl_category = 'Tools'
 
     def draw(self, context):
         layout = self.layout
         layout.operator("object.clear_vertex_groups", text="Clear All Vertex Groups", icon='GROUP_VERTEX')
         layout.operator("object.clear_modifiers", text="Clear All Modifiers", icon='MODIFIER')
+        layout.operator("object.clear_shapekeys", text="Clear All Shapekeys", icon='SHAPEKEY_DATA')
 
 
 class ClearVertexGroupsOperator(bpy.types.Operator):
@@ -90,16 +91,53 @@ class ClearModifiersOperator(bpy.types.Operator):
         return {'FINISHED'}
 
 
+class ClearShapekeysOperator(bpy.types.Operator):
+    """Clears all shapekeys from selected objects"""
+    bl_idname = "object.clear_shapekeys"
+    bl_label = "Clear All Shapekeys"
+    
+    def execute(self, context):
+        total_deleted = 0  # Inicializamos el contador
+        
+        # Obtener la lista de objetos seleccionados en la escena
+        selected_objects = context.selected_objects
+        
+        # Iterar sobre cada objeto seleccionado
+        for obj in selected_objects:
+            # Verificar si el objeto tiene shapekeys
+            if obj.data.shape_keys:
+                # Obtener la cantidad de shapekeys antes de borrarlos
+                num_shapekeys_before = len(obj.data.shape_keys.key_blocks)
+                
+                # Borrar todos los shapekeys del objeto
+                for key in obj.data.shape_keys.key_blocks:
+                    obj.shape_key_remove(key)
+                
+                # Incrementar el contador con la cantidad de shapekeys eliminados en este objeto
+                total_deleted += num_shapekeys_before
+                
+                self.report({'INFO'}, "All shapekeys deleted from object '{}'.".format(obj.name))
+            else:
+                self.report({'WARNING'}, "Skipping object '{}' as it does not have shapekeys.".format(obj.name))
+        
+        # Mostrar el mensaje con la cantidad total de shapekeys eliminados
+        self.report({'INFO'}, "Total shapekeys deleted: {}.".format(total_deleted))
+        
+        return {'FINISHED'}
+
+
 def register():
     bpy.utils.register_class(OBJECT_PT_ClearVertexGroupsPanel)
     bpy.utils.register_class(ClearVertexGroupsOperator)
     bpy.utils.register_class(ClearModifiersOperator)
+    bpy.utils.register_class(ClearShapekeysOperator)
 
 
 def unregister():
     bpy.utils.unregister_class(OBJECT_PT_ClearVertexGroupsPanel)
     bpy.utils.unregister_class(ClearVertexGroupsOperator)
     bpy.utils.unregister_class(ClearModifiersOperator)
+    bpy.utils.unregister_class(ClearShapekeysOperator)
 
 
 if __name__ == "__main__":
